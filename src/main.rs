f6c82d398671444f3c13febef5af6e1e59d8f03a
Tsvetan Tsvetkov <tsvetan.tsvetkov@efellows.bg>
2025-09-29 12:21:08 +0300

    draw cubes

---
+++ src/main.rs
@@ -0,0 +1,156 @@
+//! Here we use shape primitives to generate meshes for 3d objects as well as attaching a runtime-generated patterned texture to each 3d object.
+//!
+//! "Shape primitives" here are just the mathematical definition of certain shapes, they're not meshes on their own! A sphere with radius `1.0` can be defined with [`Sphere::new(1.0)`][Sphere::new] but all this does is store the radius. So we need to turn these descriptions of shapes into meshes.
+//!
+//! While a shape is not a mesh, turning it into one in Bevy is easy. In this example we call [`meshes.add(/* Shape here! */)`][Assets<A>::add] on the shape, which works because the [`Assets<A>::add`] method takes anything that can be turned into the asset type it stores. There's an implementation for [`From`] on shape primitives into [`Mesh`], so that will get called internally by [`Assets<A>::add`].
+//!
+//! [`Extrusion`] lets us turn 2D shape primitives into versions of those shapes that have volume by extruding them. A 1x1 square that gets wrapped in this with an extrusion depth of 2 will give us a rectangular prism of size 1x1x2, but here we're just extruding these 2d shapes by depth 1.
+//!
+//! The material applied to these shapes is a texture that we generate at run time by looping through a "palette" of RGBA values (stored adjacent to each other in the array) and writing values to positions in another array that represents the buffer for an 8x8 texture. This texture is then registered with the assets system just one time, with that [`Handle<StandardMaterial>`] then applied to all the shapes in this example.
+//!
+//! The mesh and material are [`Handle<Mesh>`] and [`Handle<StandardMaterial>`] at the moment, neither of which implement `Component` on their own. Handles are put behind "newtypes" to prevent ambiguity, as some entities might want to have handles to meshes (or images, or materials etc.) for different purposes! All we need to do to make them rendering-relevant components is wrap the mesh handle and the material handle in [`Mesh3d`] and [`MeshMaterial3d`] respectively.
+//!
+//! You can toggle wireframes with the space bar except on wasm. Wasm does not support3
+//! `POLYGON_MODE_LINE` on the gpu.
+
+use std::f32::consts::PI;
+
+#[cfg(not(target_arch = "wasm32"))]
+use bevy::pbr::wireframe::{WireframeConfig, WireframePlugin};
+use bevy::{
+    asset::RenderAssetUsages,
+    color::palettes::basic::SILVER,
+    prelude::*,
+    render::render_resource::{Extent3d, TextureDimension, TextureFormat},
+};
+
+fn main() {
+    App::new()
+        .add_plugins((
+            DefaultPlugins.set(ImagePlugin::default_nearest()),
+            #[cfg(not(target_arch = "wasm32"))]
+            WireframePlugin::default(),
+        ))
+        .add_systems(Startup, setup)
+        .add_systems(
+            Update,
+            (
+                #[cfg(not(target_arch = "wasm32"))]
+                toggle_wireframe,
+            ),
+        )
+        .run();
+}
+
+/// A marker component for our shapes so we can query them separately from the ground plane
+#[derive(Component)]
+struct Shape;
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut images: ResMut<Assets<Image>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    let debug_material = materials.add(StandardMaterial {
+        base_color: Color::srgb(160.0 / 255f32, 1.0, 0.0),
+        ..default()
+    });
+
+    let size_x = 10;
+    let size_y = 5;
+    let size_z = 8;
+
+    let mut grid = vec![vec![vec![0; size_z]; size_y]; size_x];
+
+    for i in 0..10 {
+        commands.spawn((
+            Mesh3d(meshes.add(Cuboid::new(1.0, 1.0, 1.0))),
+            MeshMaterial3d(debug_material.clone()),
+            Transform::from_xyz(
+                i as f32 * 1.2 - 6.0,
+                0.5,
+                0.0,
+            ),
+            Shape,
+        ));
+    }
+
+    commands.spawn((
+        PointLight {
+            shadows_enabled: true,
+            intensity: 10_000_000.,
+            range: 100.0,
+            shadow_depth_bias: 0.2,
+            ..default()
+        },
+        Transform::from_xyz(8.0, 16.0, 8.0),
+    ));
+
+    // ground plane
+    commands.spawn((
+        Mesh3d(meshes.add(Plane3d::default().mesh().size(50.0, 50.0).subdivisions(10))),
+        MeshMaterial3d(materials.add(Color::from(SILVER))),
+    ));
+
+    commands.spawn((
+        Camera3d::default(),
+        Transform::from_xyz(0.0, 7., 14.0).looking_at(Vec3::new(0., 1., 0.), Vec3::Y),
+    ));
+
+    #[cfg(not(target_arch = "wasm32"))]
+    commands.spawn((
+        Text::new("Press space to toggle wireframes"),
+        Node {
+            position_type: PositionType::Absolute,
+            top: Val::Px(22.0), 
+            left: Val::Px(22.0),
+            ..default()
+        },
+    ));
+}
+
+// fn rotate(mut query: Query<&mut Transform, With<Shape>>, time: Res<Time>) {
+//     for mut transform in &mut query {
+//         transform.rotate_y(time.delta_secs() / 2.);
+//     }
+// }
+
+/// Creates a colorful test pattern
+// fn uv_debug_texture() -> Image {
+//     const TEXTURE_SIZE: usize = 8;
+//
+//     let mut palette: [u8; 32] = [
+//         255, 102, 159, 255, 255, 159, 102, 255, 236, 255, 102, 255, 121, 255, 102, 255, 102, 255,
+//         198, 255, 102, 198, 255, 255, 121, 102, 255, 255, 236, 102, 255, 255,
+//     ];
+//
+//     let mut texture_data = [0; TEXTURE_SIZE * TEXTURE_SIZE * 4];
+//     for y in 0..TEXTURE_SIZE {
+//         let offset = TEXTURE_SIZE * y * 4;
+//         texture_data[offset..(offset + TEXTURE_SIZE * 4)].copy_from_slice(&palette);
+//         palette.rotate_right(4);
+//     }
+//
+//     Image::new_fill(
+//         Extent3d {
+//             width: TEXTURE_SIZE as u32,
+//             height: TEXTURE_SIZE as u32,
+//             depth_or_array_layers: 1,
+//         },
+//         TextureDimension::D2,
+//         &texture_data,
+//         TextureFormat::Rgba8UnormSrgb,
+//         RenderAssetUsages::RENDER_WORLD,
+//     )
+// }
+
+#[cfg(not(target_arch = "wasm32"))]
+fn toggle_wireframe(
+    mut wireframe_config: ResMut<WireframeConfig>,
+    keyboard: Res<ButtonInput<KeyCode>>,
+) {
+    if keyboard.just_pressed(KeyCode::Space) {
+        wireframe_config.global = !wireframe_config.global;
+    }
+}
